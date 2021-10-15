15a795fde15efa30c30d020358710c37c8449bd8
Darrell Tunnell <darrell.tunnell@googlemail.com>
2021-10-15 12:50:02 +0100

    Added logging to file anchor store - To improve observaility of file anchor operations

--- src/Stint/AnchorStore/FileSystem/FileSystemAnchorStore.cs
+++ src/Stint/AnchorStore/FileSystem/FileSystemAnchorStore.cs
@@ -5,29 +5,37 @@ namespace Stint
     using System.IO;
     using System.Threading;
     using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
 
     public class FileSystemAnchorStore : IAnchorStore
     {
         private readonly string _contentPath;
         private readonly string _name;
+        private readonly ILogger<FileSystemAnchorStore> _logger;
 
-        public FileSystemAnchorStore(string contentPath, string name)
+        public FileSystemAnchorStore(string contentPath, string name, ILogger<FileSystemAnchorStore> logger)
         {
             _contentPath = contentPath;
             _name = name;
+            _logger = logger;
         }
 
         public async Task<DateTime?> GetAnchorAsync(CancellationToken token)
         {
             var path = Path.Combine(_contentPath, _name + "-anchor.txt");
+            _logger.LogDebug("Getting anchor from {path}", path);
+
             if (File.Exists(path))
             {
                 var anchorText = await File.ReadAllTextAsync(path, token);
                 var anchorDateTime =
                     DateTime.Parse(anchorText, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
+
+                _logger.LogDebug("Anchor {anchorDateTime} loaded from file: {path}", anchorDateTime, path);
                 return anchorDateTime;
             }
 
+            _logger.LogDebug("No anchor file exists at {path}, returning null anchor.", path);
             return null;
         }
 
@@ -36,7 +44,11 @@ namespace Stint
             var path = Path.Combine(_contentPath, _name + "-anchor.txt");
             var anchor = DateTime.UtcNow;
             var anchorDateText = anchor.ToString("O");
+
+            _logger.LogDebug("Writing anchor {anchorDateTime} to anchor file: {path}", anchorDateText, path);
             await File.WriteAllTextAsync(path, anchorDateText, token);
+            _logger.LogDebug("Anchor {anchorDateText} successfully written to anchor file: {path}", anchorDateText, path);
+
             return anchor;
         }
     }
--- src/Stint/AnchorStore/FileSystem/FileSystemAnchorStoreFactory.cs
+++ src/Stint/AnchorStore/FileSystem/FileSystemAnchorStoreFactory.cs
@@ -1,10 +1,22 @@
+using Microsoft.Extensions.Logging;
+
 namespace Stint
 {
     public class FileSystemAnchorStoreFactory : IAnchorStoreFactory
     {
         private readonly string _basePath;
+        private readonly ILoggerFactory _loggerFacory;
 
-        public FileSystemAnchorStoreFactory(string basePath) => _basePath = basePath;
-        public IAnchorStore GetAnchorStore(string name) => new FileSystemAnchorStore(_basePath, name);
+        public FileSystemAnchorStoreFactory(string basePath, ILoggerFactory loggerFacory)
+        {
+            _basePath = basePath;
+            _loggerFacory = loggerFacory;
+        }
+        public IAnchorStore GetAnchorStore(string name)
+        {
+            var logger = _loggerFacory.CreateLogger<FileSystemAnchorStore>();
+            return new FileSystemAnchorStore(_basePath, name, logger);
+
+        }
     }
 }
